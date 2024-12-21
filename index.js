455d70481580593722d9ff917cebf0d0cacb1b43
Dmitry Burkovsky <dburkovsky@gmail.com>
2024-12-21 13:52:30 +0200

    App init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Hello</h1>
+        <h2>App Running test s passed</h2>
+        <p>developed by Dmytro Burkovskyi (8.1264, ZNU)<p>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`App is running on port: ${port}`);
+});
